744212a406f94aaf736def9d58c9ffb0c14f8884
Thomas B <79112289+waffleattack@users.noreply.github.com>
2023-01-13 10:22:44 -0500

    implementing FOD

--- src/main/java/frc/robot/HDrive.java
+++ src/main/java/frc/robot/HDrive.java
@@ -7,11 +7,25 @@ public class HDrive {
         VexMotorCtrl top_rgt = new VexMotorCtrl("tr",3);
         VexMotorCtrl bot_rgt = new VexMotorCtrl("br",4);
 
-        void drive(double vx, double vy, double omega) {
+        public void drive(double vx, double vy, double omega) {
                 double vl, vr, vm;
                 vl = vx - omega;
                 vr = vx + omega;
                 vm = vy;
+                
+                bot_lef.set(vl);
+                top_lef.set(vl);
+                mid.set(vm);
+                top_rgt.set(vr);
+                bot_rgt.set(vr);
+        }
+        public void FODrive(double vx, double vy, double omega, double angle){
+                double vl, vr, vm, fm,sm;
+                fm = Math.sin(angle)*vy+Math.cos(angle)*vy;
+                sm = Math.cos(angle)*vy+Math.sin(angle)*vy;
+                vl = fm - omega;
+                vr = fm + omega;
+                vm = sm;
                 
                 bot_lef.set(vl);
                 top_lef.set(vl);
