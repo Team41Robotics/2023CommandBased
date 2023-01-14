2d6eb25f7b535d5ebe1f0a7ea05403a6f94c3204
Mason DiGiorgio <mason@digiorgiofamily.net>
2023-01-14 10:53:02 -0500

    FOD drive modes

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -14,6 +14,7 @@ import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 public class Robot extends TimedRobot {
         Joystick left_js = new Joystick(1);
         Joystick right_js = new Joystick(0);
+        private boolean FOD;
 
         HDrive hdrive = new HDrive();
         ShuffleboardTab camtab = Shuffleboard.getTab("Camera");
@@ -78,22 +79,36 @@ public class Robot extends TimedRobot {
 
         @Override
         public void teleopPeriodic() {
-                double vf = -left_js.getY();
-                if(Math.abs(vf) < 0.1) vf=0;
-                double vs = -left_js.getX();
-                if(Math.abs(vs) < 0.1) vs=0;
-                double omega = right_js.getX();
-                if(Math.abs(omega) < 0.1) omega=0;
+
+
+                
+                double vf = -deadZone(left_js.getY());
+                double vs = -deadZone(left_js.getX());
+                System.out.println(deadZone(left_js.getY()));
+                double omega = deadZone(right_js.getX());
+                if(Math.abs(omega) < 0.5) omega=0;
                 double robot_angle = imu.ahrs.getYaw();
                 double vx = vf * Math.cos(robot_angle*Math.PI/180) - vs * Math.sin(robot_angle*Math.PI/180);
                 double vy = vf * Math.sin(robot_angle*Math.PI/180) + vs * Math.cos(robot_angle*Math.PI/180);
                 // robot centric drive
                 //hdrive.drive(-left_js.getY(), left_js.getX(), -right_js.getX());
                 //hdrive.drive(-vx,vy,-omega);
+                if(right_js.getRawButtonPressed(2))
+                        FOD = !FOD;
+
                 if(left_js.getRawButton(2)) imu.ahrs.zeroYaw();
                 if(left_js.getRawButton(1)){
                         hdrive.drive(0.5 * Math.signum(imu.ahrs.getPitch())*Math.sqrt(Math.abs(imu.ahrs.getPitch() / 15)),0,0);
-                }else   hdrive.drive(-vx,vy,-omega);
+                }else if(FOD) {hdrive.drive(-vx,vy,-omega);}
+                else{
+                        hdrive.drive(vf, -vs, -omega);
+                } 
+
                 //hdrive.drive( left_js.getY() * (left_js.getRawButton(0)? imu.angle/15: 1),0,0);
+        }
+
+
+        private double deadZone(double joystickAxis){
+                return ((Math.abs(joystickAxis)>0.2)?joystickAxis:0);
         }
 }
