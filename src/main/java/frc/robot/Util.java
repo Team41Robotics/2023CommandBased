7b550a55236f9866dc8201029302813c5710ec30
Jason Zeng <jasonzeng124@gmail.com>
2023-01-27 15:13:31 -0500

    format properly this time :)

--- src/main/java/frc/robot/Util.java
+++ src/main/java/frc/robot/Util.java
@@ -1,29 +1,29 @@
 package frc.robot;
 
 public class Util {
-  public static double deadZone(double joystickAxis) {
-    return Math.abs(joystickAxis) > 0.2 ? joystickAxis : 0;
-  }
+        public static double deadZone(double joystickAxis) {
+                return Math.abs(joystickAxis) > 0.2 ? joystickAxis : 0;
+        }
 
-  public static Transform2d interpolate(Transform2d a, Transform2d b, double alpha) {
-    // linear interpolate; (1-alpha) a + alpha b
-    double ra = a.getTheta();
-    double rb = b.getTheta();
-    double ro = ra + normRot(rb - ra) * alpha;
+        public static Transform2d interpolate(Transform2d a, Transform2d b, double alpha) {
+                // linear interpolate; (1-alpha) a + alpha b
+                double ra = a.getTheta();
+                double rb = b.getTheta();
+                double ro = ra + normRot(rb - ra) * alpha;
 
-    // Translation2d ta = a.getTranslation();
-    // Translation2d tb = b.getTranslation();
-    // Translation2d to = ta.times(1 - alpha).plus(tb.times(alpha));
-    double tx = a.getX() + b.getX();
-    double ty = a.getY() + b.getY();
-    return new Transform2d(tx, ty, ro);
-  }
+                // Translation2d ta = a.getTranslation();
+                // Translation2d tb = b.getTranslation();
+                // Translation2d to = ta.times(1 - alpha).plus(tb.times(alpha));
+                double tx = a.getX() + b.getX();
+                double ty = a.getY() + b.getY();
+                return new Transform2d(tx, ty, ro);
+        }
 
-  public static double normRot(double rad) {
-    rad %= 2 * Math.PI;
-    rad += 2 * Math.PI;
-    rad %= 2 * Math.PI;
-    if (rad > Math.PI) rad -= 2 * Math.PI;
-    return rad;
-  }
+        public static double normRot(double rad) {
+                rad %= 2 * Math.PI;
+                rad += 2 * Math.PI;
+                rad %= 2 * Math.PI;
+                if (rad > Math.PI) rad -= 2 * Math.PI;
+                return rad;
+        }
 }
--- src/main/java/frc/robot/commands/FODdrive.java
+++ src/main/java/frc/robot/commands/FODdrive.java
@@ -8,29 +8,29 @@ import frc.robot.subsystems.HDriveSubsystem;
 import frc.robot.subsystems.OdomSubsystem;
 
 public class FODdrive extends CommandBase {
-  Joystick leftjs, rightjs;
-  HDriveSubsystem drive = HDriveSubsystem.getInstance();
-  OdomSubsystem odom = OdomSubsystem.getInstance();
+        Joystick leftjs, rightjs;
+        HDriveSubsystem drive = HDriveSubsystem.getInstance();
+        OdomSubsystem odom = OdomSubsystem.getInstance();
 
-  public FODdrive() {
-    addRequirements(drive);
-    leftjs = Robot.leftjs;
-    rightjs = Robot.rightjs;
-  }
+        public FODdrive() {
+                addRequirements(drive);
+                leftjs = Robot.leftjs;
+                rightjs = Robot.rightjs;
+        }
 
-  public void execute() {
-    double vf = -Util.deadZone(leftjs.getY());
-    double vs = -Util.deadZone(leftjs.getX());
-    double w = -Util.deadZone(rightjs.getX());
-    double robot_angle = odom.now().getTheta();
-    double vx = Math.cos(robot_angle) * vf + Math.sin(robot_angle) * vs;
-    double vy = -Math.sin(robot_angle) * vf + Math.cos(robot_angle) * vs;
-    drive.drive(vx, vy, w);
-  }
+        public void execute() {
+                double vf = -Util.deadZone(leftjs.getY());
+                double vs = -Util.deadZone(leftjs.getX());
+                double w = -Util.deadZone(rightjs.getX());
+                double robot_angle = odom.now().getTheta();
+                double vx = Math.cos(robot_angle) * vf + Math.sin(robot_angle) * vs;
+                double vy = -Math.sin(robot_angle) * vf + Math.cos(robot_angle) * vs;
+                drive.drive(vx, vy, w);
+        }
 
-  @Override
-  public boolean isFinished() {
-    return true;
-    // trivial
-  }
+        @Override
+        public boolean isFinished() {
+                return true;
+                // trivial
+        }
 }
