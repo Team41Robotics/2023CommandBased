2d3b23a60dbcd11718b74a421b4517c329b99eb1
Ben Phan <whippedcreamtoast@gmail.com>
2023-01-30 16:07:32 -0500

    Setting up stuff for the Arm

--- src/main/java/frc/robot/Arm.java
+++ src/main/java/frc/robot/Arm.java
@@ -1,19 +1,21 @@
 package frc.robot;
 
-import edu.wpi.first.wpilibj.Encoder;
+import java.util.Math;
+
 import edu.wpi.first.wpilibj.DigitalInput;
- 
+
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkMaxPIDController;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.SparkMaxRelativeEncoder;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 
 public class Arm {
 
-    private static Encoder motor1Encoder;
-    private static Encoder motor2Encoder;
-    private static Encoder motor3Encoder;
-
-    private static VexMotorCtrl motor1;
-    private static VexMotorCtrl motor2;
-    private static VexMotorCtrl motor3;
+    private static CANSparkMax motor1;
+    private static CANSparkMax motor2;
+    private static CANSparkMax motor3;
 
     private static final int  length1 = 10;
     private static final int length2 = 5;
@@ -24,23 +26,64 @@ public class Arm {
     private static DigitalInput originLimit;
     private static DigitalInput maximumLimit;
 
+    private static RelativeEncoder motor1Encoder;
+    private static RelativeEncoder motor2Encoder;
+    private static RelativeEncoder motor3Encoder;
+
+    SparkMaxPIDController motor1PIDController;
+    SparkMaxPIDController motor2PIDController;
+    SparkMaxPIDController motor3PIDController;
+
     public void initArm(){
 
-        motor1Encoder = new Encoder(0,1,true);
-        motor2Encoder = new Encoder(2,3,true);
-        motor3Encoder = new Encoder(4,5,true);
-
-        motor1 = new VexMotorCtrl("motor1", 1);
-        motor2 = new VexMotorCtrl("motor2", 2);
-        motor3 = new VexMotorCtrl("motor3", 3);
+        motor1 = new CANSparkMax(Constants.NEO_ONE_DEVICE_ID, MotorType.kBrushless);
+        motor2 = new CANSparkMax(Constants.NEO_TWO_DEVICE_ID, MotorType.kBrushless);
+        motor3 = new CANSparkMax(Constants.NEO_THREE_DEVICE_ID, MotorType.kBrushless);
         
+        motor1Encoder = motor1.getEncoder(SparkMaxRelativeEncoder.Type.kQuadrature, 4096);
+        motor2Encoder = motor2.getEncoder(SparkMaxRelativeEncoder.Type.kQuadrature, 4096);
+        motor3Encoder = motor3.getEncoder(SparkMaxRelativeEncoder.Type.kQuadrature, 4096);  
+
         originLimit = new DigitalInput(0);
         maximumLimit = new DigitalInput(1);
 
-        motor1Encoder.reset();
-        motor2Encoder.reset();
-        motor3Encoder.reset();
+        if(!originLimit.get()){
+            motor1Encoder.reset();
+            motor2Encoder.reset();
+            motor3Encoder.reset();
+        }
+
+        motor1.restoreFactoryDefaults();
+        motor2.restoreFactoryDefaults();
+        motor3.restoreFactoryDefaults();
+
+        motor1PIDController = motor1.getPIDController();
+        motor2PIDController = motor2.getPIDController();
+        motor3PIDController = motor3.getPIDController();
+
+        setPID(motor1PIDController, Constants.kP, Constants.kI, Constants.kD);
+        setPID(motor2PIDController, Constants.kP, Constants.kI, Constants.kD);
+        setPID(motor3PIDController, Constants.kP, Constants.kI, Constants.kD);
+
+        motor1Encoder.setConversionFactor();
+        motor2Encoder.setConversionFactor();
+        motor3Encoder.setConversionFactor();
+
+        }
+
 
     }
 
-}
+    public void setPID(SparkMaxPIDController controller, double p, double i, double d){
+        controller.setP(p);
+        controller.setI(i);
+        controller.setD(d);
+    }
+
+    // WIP
+
+    public double calculateVelocity (RelativeEncoder encoder){
+        return -1;
+    }
+
+}   
--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -16,4 +16,19 @@ public final class Constants {
                 public static final int FRONT_RIGHT_MOTOR_CHANNEL = 3;
                 public static final int MID_MOTOR_CHANNEL = 2;
         }
+
+        public static class ArmConstants {
+
+                public static final int NEO_ONE_DEVICE_ID = 1;
+                public static final int NEO_TWO_DEVICE_ID = 2;
+                public static final int NEO_THREE_DEVICE_ID = 3;
+
+                public static final double kP = 0.1;
+                public static final double kI = 0;
+                public static final double kD = 1;
+                
+                public static final double JOINT_RADIUS;
+
+        }
+
 }
