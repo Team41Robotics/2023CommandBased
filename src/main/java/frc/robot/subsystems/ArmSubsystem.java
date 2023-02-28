60734c477b9caf69668676ed4146e437284ad471
Jason Zeng <jasonzeng124@gmail.com>
2023-02-28 14:44:41 -0500

    minor refactor

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -12,18 +12,18 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ArmSubsystem extends SubsystemBase {
 	CANSparkMax elev = new CANSparkMax(ELEV_ID, MotorType.kBrushless);
-	CANSparkMax joint1 = new CANSparkMax(JOINT1_ID, MotorType.kBrushless);
-	CANSparkMax joint2 = new CANSparkMax(JOINT2_ID, MotorType.kBrushless);
+	CANSparkMax jt1 = new CANSparkMax(JOINT1_ID, MotorType.kBrushless);
+	CANSparkMax jt2 = new CANSparkMax(JOINT2_ID, MotorType.kBrushless);
 
 	SparkMaxPIDController elev_vpid = elev.getPIDController();
-	SparkMaxPIDController joint1_vpid = joint1.getPIDController();
-	SparkMaxPIDController joint2_vpid = joint2.getPIDController();
+	SparkMaxPIDController jt1_vpid = jt1.getPIDController();
+	SparkMaxPIDController jt2_vpid = jt2.getPIDController();
 
 	public ArmSubsystem() {
 		zero();
 		setPID(elev_vpid, 1, 0, 0, 0);
-		setPID(joint1_vpid, 1, 0, 0, 0);
-		setPID(joint2_vpid, 1, 0, 0, 0);
+		setPID(jt1_vpid, 1, 0, 0, 0);
+		setPID(jt2_vpid, 1, 0, 0, 0);
 	}
 
 	private void setPID(SparkMaxPIDController pid, double kP, double kI, double kD, double kIz) {
@@ -35,49 +35,37 @@ public class ArmSubsystem extends SubsystemBase {
 
 	public void zero() {
 		elev.getEncoder().setPosition(ELEV_PACKAGED_POSITION / 2 / PI / ELEV_METERS_PER_RAD);
-		joint1.getEncoder().setPosition(JOINT1_PACKAGED_POSITION / 2 / PI / JOINT1_RATIO);
-		joint2.getEncoder().setPosition(JOINT2_PACKAGED_POSITION / 2 / PI / JOINT2_RATIO);
+		jt1.getEncoder().setPosition(JOINT1_PACKAGED_POSITION / 2 / PI / JOINT1_RATIO);
+		jt2.getEncoder().setPosition(JOINT2_PACKAGED_POSITION / 2 / PI / JOINT2_RATIO);
 
 		elev.restoreFactoryDefaults();
-		joint1.restoreFactoryDefaults();
-		joint2.restoreFactoryDefaults();
+		jt1.restoreFactoryDefaults();
+		jt2.restoreFactoryDefaults();
 	}
 	// TODO dynamic zeroing of encoder based on limit switches on elevator
 
-	public void set(double elev_vel, double joint1_vel, double joint2_vel) {
-		set(elev_vel, joint1_vel, joint2_vel, 0, 0, 0);
+	public void set(double elev_vel, double jt1_vel, double jt2_vel) {
+		set(elev_vel, jt1_vel, jt2_vel, 0, 0, 0);
 	}
 
-	public void set(
-			double elev_vel,
-			double joint1_vel,
-			double joint2_vel,
-			double elev_acc,
-			double joint1_acc,
-			double joint2_acc) {
+	public void set(double elev_v, double jt1_v, double jt2_v, double elev_a, double jt1_a, double jt2_a) {
 		elev_vpid.setReference(
-				elev_vel / ELEV_METERS_PER_RAD / 2 / PI,
+				elev_v / ELEV_METERS_PER_RAD / 2 / PI,
 				ControlType.kVelocity,
 				0,
-				ELEV_kG + ELEV_kS * signum(elev_vel) + ELEV_kV * elev_vel + ELEV_kA * elev_acc,
+				ELEV_kG + ELEV_kS * signum(elev_v) + ELEV_kV * elev_v + ELEV_kA * elev_a,
 				ArbFFUnits.kVoltage);
-		joint1_vpid.setReference(
-				joint1_vel * JOINT1_RATIO / 2 / PI,
+		jt1_vpid.setReference(
+				jt1_v * JOINT1_RATIO / 2 / PI,
 				ControlType.kVelocity,
 				0,
-				JOINT1_kG * cos(getJoint1Pos())
-						+ JOINT1_kS * signum(joint1_vel)
-						+ JOINT1_kV * joint1_vel
-						+ JOINT1_kA * joint1_acc,
+				JOINT1_kG * cos(getJoint1Pos()) + JOINT1_kS * signum(jt1_v) + JOINT1_kV * jt1_v + JOINT1_kA * jt1_a,
 				ArbFFUnits.kVoltage);
-		joint2_vpid.setReference(
-				joint2_vel * JOINT2_RATIO / 2 / PI,
+		jt2_vpid.setReference(
+				jt2_v * JOINT2_RATIO / 2 / PI,
 				ControlType.kVelocity,
 				0,
-				JOINT2_kG * cos(getJoint2Pos())
-						+ JOINT2_kS * signum(joint2_vel)
-						+ JOINT2_kV * joint2_vel
-						+ JOINT2_kA * joint2_acc,
+				JOINT2_kG * cos(getJoint2Pos()) + JOINT2_kS * signum(jt2_v) + JOINT2_kV * jt2_v + JOINT2_kA * jt2_a,
 				ArbFFUnits.kVoltage);
 	}
 
@@ -91,10 +79,10 @@ public class ArmSubsystem extends SubsystemBase {
 	}
 
 	public double getJoint1Pos() {
-		return joint1.getEncoder().getPosition() * 2 * PI / JOINT1_RATIO;
+		return jt1.getEncoder().getPosition() * 2 * PI / JOINT1_RATIO;
 	}
 
 	public double getJoint2Pos() {
-		return joint2.getEncoder().getPosition() * 2 * PI / JOINT2_RATIO;
+		return jt2.getEncoder().getPosition() * 2 * PI / JOINT2_RATIO;
 	}
 }
