c65a93f0f1e7f3c68cdec7e817481d40645855d1
Jason Zeng <jasonzeng124@gmail.com>
2023-03-23 11:00:23 -0400

    imagine javadocs
    sorry thomas

--- src/main/java/frc/robot/util/Util.java
+++ src/main/java/frc/robot/util/Util.java
@@ -10,60 +10,32 @@ import frc.robot.constants.Constants;
 import frc.robot.constants.Constants.OperatorConstants;
 
 public class Util {
-	/**
-	 * @param joystickAxis joystick axis input
-	 * @param deadZone dead zone
-	 * @return applied deadzone
-	 */
 	public static double deadZone(double joystickAxis, double deadZone) {
 		if (abs(joystickAxis) < deadZone) return 0;
 		return signum(joystickAxis) * (abs(joystickAxis) - deadZone) / (1 - deadZone);
 	}
 
-	/**
-	 * @param joystickAxis joystick input
-	 * @param deadZone dead zone
-	 * @return applied deadzone & sense curve
-	 */
+	public static double deadZone(double joystickAxis) {
+		return deadZone(joystickAxis, OperatorConstants.JOYSTICK_DEADZONE);
+	}
+
 	public static double curvedDeadZone(double joystickAxis, double deadZone) {
 		double deaded = deadZone(joystickAxis, deadZone);
 		return deaded * abs(deaded); // TODO maybe change curve later
 	}
 
-	/**
-	 * @param joystickAxis joystick input
-	 * @return applied deadzone & sense curve
-	 */
 	public static double curvedDeadZone(double joystickAxis) {
 		return curvedDeadZone(joystickAxis, OperatorConstants.JOYSTICK_DEADZONE);
 	}
 
-	/**
-	 * @param joystickAxis joystick axis input
-	 * @return applied deadzone with default deadzone
-	 */
-	public static double deadZone(double joystickAxis) {
-		return deadZone(joystickAxis, OperatorConstants.JOYSTICK_DEADZONE);
-	}
-
-	/**
-	 * @param rad
-	 * @return rotation normalized from -180 deg to 180 deg
-	 */
 	public static double normRot(double rad) {
 		rad %= 2 * PI;
-		rad += 2 * PI;
-		rad %= 2 * PI;
 		if (rad > PI) rad -= 2 * PI;
+                if (rad < -PI) rad += 2 * PI;
 		return rad;
 	}
 
-	/**
-	 * @param t1 1st transform to be lerped
-	 * @param t2 2nd transform to be lerped
-	 * @param alpha alpha value for lerping (0=t1; 1=t2)
-	 * @return
-	 */
+        // lerps between t1 and t2; 0=t1, 1=t2
 	public static Transform2d lerp(Transform2d t1, Transform2d t2, double alpha) {
 		double tx = (1 - alpha) * t1.x + alpha * t2.x;
 		double ty = (1 - alpha) * t1.y + alpha * t2.y;
@@ -72,7 +44,7 @@ public class Util {
 		return new Transform2d(tx, ty, theta);
 	}
 
-	public static Transform2d flipTransformAcrossField(Transform2d trans) {
+	public static Transform2d flipPoseAcrossField(Transform2d trans) {
 		return new Transform2d(Constants.FIELD_LENGTH - trans.x, trans.y, PI - trans.theta);
 	}
 
